19e181c2fd40d11b89290cf13d4a529f7591dd2c
Jake Southwell <southwelljake@hotmail.co.uk>
2022-03-03 13:53:21 +0000

    Class to store solar panel variables

--- src/solarPanel.py
+++ src/solarPanel.py
@@ -6,15 +6,4 @@ class SolarPanel:
 
         self.efficiency = efficiency
         self.area = area
-
-        self.P_solar = 0
-
-        # Input states
-        self.irradiance = 0
-        self.cloud_cover = 0
-
-    def update(self):
-
-        self.P_solar = self.efficiency * self.area * self.irradiance * \
-                       (1 - 0.75 * self.cloud_cover ** 0.75)
-
+        self.power = 0
