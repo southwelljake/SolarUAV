791471c7225eae36500216768a70e9a8be3c3c02
Jake Southwell <southwelljake@hotmail.co.uk>
2022-04-08 16:11:35 +0100

    Added thrust function from main script

--- src/propeller.py
+++ src/propeller.py
@@ -5,3 +5,12 @@ class Propeller:
 
         self.efficiency = efficiency  # Efficiency of propellers
         self.power = 0  # Power to propellers (W)
+
+    def calculate_propeller_thrust(self, v_air, state_var):
+        # Compute thrust
+        if v_air != 0 and state_var[6] > 0:
+            thrust = abs(self.power) * self.efficiency / v_air
+        else:
+            thrust = 0
+
+        return thrust
